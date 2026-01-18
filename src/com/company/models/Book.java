beff60b60db4b43cc3ad0ee29473d42c4787725c
aruzhan <a-kenzhebulatova@mail.ru>
2026-01-18 22:12:31 +0600

    Add Book model and repository

---
+++ src/com/company/models/Book.java
@@ -0,0 +1,45 @@
+package com.company.models;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int availableCopies;
+
+    public Book(int id, String title, String author, int availableCopies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.availableCopies = availableCopies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", availableCopies=" + availableCopies +
+                '}';
+    }
+}
+
